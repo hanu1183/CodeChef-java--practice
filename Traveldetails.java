83fb16f8b13670001121949803ece4f6042dc52a
hanu1183 <ketanakemburi@gmail.com>
2026-08-12 07:31:38 -0700

    Create Traveldetails.java

---
+++ Traveldetails.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign city distance in long
+long citydistance= 450;
+double converted_distance= citydistance;
+boolean is_journey_active = true;
+System.out.println("Distance in Long: "+citydistance+" km");
+System.out.println("Converted Distance in Double: "+converted_distance+" km");
+System.out.println("Is Journey Active: "+is_journey_active);
+
+
+        
+    }
+}

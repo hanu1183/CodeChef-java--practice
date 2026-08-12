b62cc52feb33848857a205c888684d1f851501c2
hanu1183 <ketanakemburi@gmail.com>
2026-08-12 04:59:36 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign multiple int variables in one statement
+int gs=3,am=2,sot=5;
+ double pr1=8.5,pr2=7.9;
+System.out.println("Goals Scored: "+gs);
+System.out.println("Assists Made: "+am);
+System.out.println("Shots on Target: "+sot);
+System.out.println("Player 1 Rating: "+pr1);
+System.out.println("Player 2 Rating: "+7.9);
+}
+}
+
+        // Print all values

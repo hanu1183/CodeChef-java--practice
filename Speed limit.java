f4c5ac74cb6067f35eaaac90f17aaa58fac06bc6
hanu1183 <ketanakemburi@gmail.com>
2026-08-12 08:08:43 -0700

    Create Speed limit.java

---
+++ Speed limit.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign a fixed speed limit using final
+final int SPEED_LIMIT = 80;
+int currentSpeed = 75;
+
+        // Declare and assign current speed
+
+
+        // Print the speed limit and current speed
+        System.out.println("Speed Limit: " + SPEED_LIMIT + " km/h");
+        System.out.println("Current Speed: " + currentSpeed + " km/h");
+    }
+}

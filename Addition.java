05acd91d28724d8226c37a39d6432cf5d2b1eb0f
hanu1183 <ketanakemburi@gmail.com>
2026-08-12 21:08:48 -0700

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Write your code here
+        int pushups = 40, situps = 30, squats = 20;
+        int sum = pushups+situps+squats;
+        System.out.println(sum);
+    }
+}

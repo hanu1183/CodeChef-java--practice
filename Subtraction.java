8c8aa656754f067d5faaf2ffcb8656544952afae
hanu1183 <ketanakemburi@gmail.com>
2026-08-12 21:20:13 -0700

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare integer variables for poster inventory
+        int totalPosters = 200, removedPosters = 75, newPosters = 40;
+        
+        // Calculate the updated stock
+int difference = totalPosters-removedPosters;
+int updatedStock = newPosters+difference;
+System.out.println(updatedStock);
+        // Print the updated stock
+
+    }
+}

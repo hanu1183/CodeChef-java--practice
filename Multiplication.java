6e0850f9efcd7c51360f40294575ff234c8ef052
hanu1183 <ketanakemburi@gmail.com>
2026-08-12 21:36:15 -0700

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+
+         // Given Variables
+        int sponsors = 4, donationPerSponsor = 4500, extraDonations = 8000;
+       int product = sponsors*donationPerSponsor;
+       int total_amount = product+extraDonations;
+       System.out.println(total_amount);
+    }
+}

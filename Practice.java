cb06fb081e249679c98357d9f6f17f65d775750d
hanu1183 <ketanakemburi@gmail.com>
2026-08-13 03:06:34 -0700

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,25 @@
+class Codechef {
+    
+    //  Define the main method
+    public static void main(String[] args) {
+        
+        //  Declare an integer variable for the vehicle number
+        int vehicleNumber = 278; 
+        int lastDigit = vehicleNumber%10;
+        int evenOdd = 278%2;
+        System.out.println("Last digit of vehicle number: "+lastDigit);
+        System.out.println("Even/Odd check (0 = Even, 1 = Odd): "+evenOdd);
+        //  Declare variables lastDigit and evenOdd to store the output.
+
+
+
+        //  Extract the last digit using modulus
+
+
+        //  Check if the number is even or odd using modulus
+
+
+        //  Print the results
+        
+    }
+}

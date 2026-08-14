67f6e3935183b54274061a1ed5533c5a14b7e5bf
hanu1183 <ketanakemburi@gmail.com>
2026-08-14 08:28:23 -0700

    Create Secondassignment.java

---
+++ Secondassignment.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        float fuelLevel = 45.5f, fuelConsumed = 12.3f;
+        // Cook your dish here
+        float nf = fuelLevel;
+        nf -= fuelConsumed;
+        System.out.println(nf);
+    }
+}

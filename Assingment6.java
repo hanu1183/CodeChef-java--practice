3ac0f0b2b52ea8cdab2d8fce23074a6320ba24a7
hanu1183 <ketanakemburi@gmail.com>
2026-08-14 09:15:19 -0700

    Create Assingment6.java

---
+++ Assingment6.java
@@ -0,0 +1,20 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Given variables: Initial stock, new stock added, and damaged units
+        int initialStock = 500, newStock = 200, damagedUnits = 50, storageA, storageB, storageC;
+
+        // Adjust initialStock by adding newStock and subtracting damagedUnits
+int kc = initialStock+newStock;
+int rc = kc - damagedUnits;
+ storageA=storageB=storageC=rc;
+System.out.println(storageA);
+System.out.println(storageB);
+System.out.println(storageC);
+
+        // Assign the final stock count to all storage locations using chained assignment
+
+
+        // Print updated stock values in all storage locations
+        
+    }
+}

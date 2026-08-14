4faa33f69279299cb49b00e0aa63e28b4af47e33
hanu1183 <ketanakemburi@gmail.com>
2026-08-14 08:21:02 -0700

    Create Bankcalc.java

---
+++ Bankcalc.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args){
+        
+        double accountBalance = 5000.50, interest = 250.75;
+        // Cook your dish here
+         double ub = accountBalance;
+         ub += interest;
+        System.out.println(ub);
+        
+    }
+}

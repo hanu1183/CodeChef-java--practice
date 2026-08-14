76797fec302abbea7c28bbea81714ea0c5284ce9
hanu1183 <ketanakemburi@gmail.com>
2026-08-14 08:36:20 -0700

    Create Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Cook your dish here
+        double salary = 45000.75, bonusMultiplier = 1.10;
+        double ns = salary;
+        ns *= bonusMultiplier;
+        System.out.println(ns);
+    }
+}

3fc2aa2c4b40de7dc5409cfeef9b16adf3cbc850
hanu1183 <ketanakemburi@gmail.com>
2026-08-16 01:16:18 -0700

    Implement main method to calculate total cost

---
+++ Operator.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    // Write itemPrice here
+int itemPrice = 5;
+    // Write shippingCost here
+int shippingCost = 7;
+    // Write sum here (itemPrice + shippingCost)
+int sum = itemPrice+shippingCost;
+    // Print sum here
+System.out.println(sum);
+  }
+}

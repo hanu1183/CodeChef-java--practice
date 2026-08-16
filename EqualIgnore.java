637b7a81d9a116d0a0011df286e12fab39a4013a
hanu1183 <ketanakemburi@gmail.com>
2026-08-16 08:29:35 -0700

    Create EqualIgnore.java for review comparison
    Added a Java class to compare product reviews ignoring case sensitivity.

---
+++ EqualIgnore.java
@@ -0,0 +1,15 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Given Variables
+        String review1 = "Excellent Product";
+        String review2 = "excellent product";
+        double productPrice = 249.99;
+        int productId = 1024;
+        boolean isMatch = review1.equalsIgnoreCase(review2);
+System.out.println("Product ID: "+productId);
+System.out.println("Product Price: "+"$"+productPrice);
+System.out.println("Review Match: "+isMatch);
+        // Complete the below code
+        
+    }
+}

afad28682c2a34bff70b11a3b8ff54cf081637c9
hanu1183 <ketanakemburi@gmail.com>
2026-08-17 11:07:02 -0700

    Implement discount eligibility check in Java
    Added a Java program to check discount eligibility based on purchase amount.

---
+++ Prac.java/If.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking user input for purchase amount
+int totalpurchase = scanner.nextInt();
+
+
+        // Checking discount eligibility
+if (totalpurchase >= 1000){
+    System.out.println("Congratulations! You are eligible for a discount.");
+}
+
+        scanner.close();
+    }
+}

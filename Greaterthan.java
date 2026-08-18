c29b8650cb6843e4ffea98d9bdb3a0b572365618
hanu1183 <ketanakemburi@gmail.com>
2026-08-18 13:42:52 -0400

    Create Greaterthan.java

---
+++ Greaterthan.java
@@ -0,0 +1,19 @@
+import java.util.*;
+// This class demonstrates the use of statements and semicolons in Java.
+class Codechef {
+
+    // The main method serves as the entry point for the program.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int studentscore= sc.nextInt();
+        if (studentscore>60) {
+            System.out.println("You are eligible for the advanced course!");
+             } else {
+                 System.out.println("You need to score higher to enroll in the advanced course.");
+             }
+        
+        
+        
+        // write your code here
+    }
+}

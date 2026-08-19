87b115533864b27f9adc524ea738abd0361fdcda
hanu1183 <ketanakemburi@gmail.com>
2026-08-19 01:25:55 -0400

    Add DiscountCheck class to evaluate age for discounts
    This program checks if a user is eligible for a senior citizen discount based on their age input.

---
+++ DiscountCheck.java
@@ -0,0 +1,21 @@
+import java.util.*;
+// This class demonstrates the use of statements and semicolons in Java.
+class Codechef {
+
+    // The main method serves as the entry point for the program.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age >= 60 )
+        { 
+            System.out.println("You are eligible for a senior citizen discount!");
+             System.out.println("Enjoy your discounted movie ticket.");
+        } else { 
+            System.out.println("You are not eligible for a senior citizen discount.");
+             System.out.println("You need to wait 10 more years to qualify");
+        }
+        
+        
+        // write your code here
+    }
+}

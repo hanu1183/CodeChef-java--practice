7cfe67f9e8a6b98626fdbbadea8273db6ef40a0a
hanu1183 <ketanakemburi@gmail.com>
2026-08-19 02:22:00 -0400

    Create MovieNightEligibilityCheck.java

---
+++ MovieNightEligibilityCheck.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking user input for delivery conditions
+
+boolean destination = scanner.nextBoolean();
+int ordertime = scanner.nextInt();
+if (destination||ordertime<2) {
+    System.out.println("Package qualifies for same-day delivery.");
+} else {
+    System.out.println("Package does not qualify for same-day delivery.");
+}
+        // Checking eligibility for same-day delivery
+ }
+}

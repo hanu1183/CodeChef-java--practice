b855a038b05e98840bcbf054ee8a04abd4259741
hanu1183 <ketanakemburi@gmail.com>
2026-08-20 13:26:58 -0400

    Add Result class to display course information

---
+++ Result.java
@@ -0,0 +1,38 @@
+package module;
+
+public class Result {public static void main(String[] args)
+	{
+String scid="1233";
+String scname="java";
+byte scred=4;
+String descp="programming";
+short deptid=1200;
+int attenid=2134;
+String date="10-18-2026";
+String status="update";
+int tclass=7;
+int cattended=5;
+double attendper=85.55;
+int resultid=644;
+double marks=65.77;
+String grade="a+";
+double intermarks=45.3;
+double extmarks=23.8;
+System.out.println("Course id="+scid);
+System.out.println("Course name="+scname);
+System.out.println("Credits="+scred);
+System.out.println("Description="+descp);
+System.out.println("Department id="+deptid);
+System.out.println("Attendance id="+attenid);
+System.out.println("Date="+date);
+System.out.println("Status="+status);
+System.out.println("Total classes="+tclass);
+System.out.println("Classes attended="+cattended);
+System.out.println("Attendance percentage="+attendper);
+System.out.println("Result id="+resultid);
+System.out.println("Marks="+marks);
+System.out.println("Grade="+grade);
+System.out.println("Internal marks="+intermarks);
+System.out.println("external marks="+extmarks);
+	}
+}
